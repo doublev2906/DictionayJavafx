a1d38b2a06b27f588f033fe5e71e190dcde24acc
doublev2906 <83346733+doublev2906@users.noreply.github.com>
2021-09-21 11:38:35 +0700

    Add files via upload

---
+++ dictionary/src/sample/Word.java
@@ -0,0 +1,25 @@
+package sample;
+
+public class Word {
+    private String engWord;
+    private String vnWord;
+
+
+    public String getEngWord() {
+        return engWord;
+    }
+
+    public void setEngWord(String engWord) {
+        this.engWord = engWord;
+    }
+
+    public String getVnWord() {
+        return vnWord;
+    }
+
+    public void setVnWord(String vnWord) {
+        this.vnWord = vnWord;
+    }
+
+
+}
